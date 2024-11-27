1a17c1a95c92ff7c66e4b46eb204d7d83dafcb9f
Georgii Boldyrev <nordic3800@gmail.com>
2024-11-27 19:38:41 +0300

    task_1 done

---
+++ task_1/task_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p House/first_floor/grandmothers_bedroom
+mkdir -p House/first_floor/fathers_bedroom
+mkdir -p House/first_floor/mothers_bedroom
+mkdir -p House/first_floor/daughters_bedroom
+mkdir -p House/first_floor/sons_bedroom
+mkdir -p House/ground_floor/lobby
+mkdir -p House/ground_floor/living_room
+mkdir -p House/ground_floor/kitchen
+echo "wardrobe" > House/ground_floor/lobby/furniture.txt
+echo "wardrobe" > House/ground_floor/living_room/furniture.txt 
+echo "sofa" >> House/ground_floor/living_room/furniture.txt 
