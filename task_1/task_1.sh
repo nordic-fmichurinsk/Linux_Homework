e4159921899f02b17d90de774090be4612ebce69
Georgii Boldyrev <nordic3800@gmail.com>
2024-11-27 19:42:06 +0300

    task_1 modified

--- task_1/task_1.sh
+++ task_1/task_1.sh
@@ -10,3 +10,4 @@ mkdir -p House/ground_floor/kitchen
 echo "wardrobe" > House/ground_floor/lobby/furniture.txt
 echo "wardrobe" > House/ground_floor/living_room/furniture.txt 
 echo "sofa" >> House/ground_floor/living_room/furniture.txt 
+echo "table" >> House/ground_floor/living_room/furniture.txt 
